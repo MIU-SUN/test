2318a362650c68843fca0f44e246e8120f4bf281
me <602171719@qq.com>
2023-06-07 01:26:08 +0800

    测试提交

---
+++ src/main/java/com/wmh/bean/User.java
@@ -0,0 +1,16 @@
+package com.wmh.bean;
+
+/**
+ * @ClassName: User
+ * @Description: TODO
+ * @Author: isiy
+ * @Date: 2023/6/3 0:11
+ * @Version: 1.0
+ **/
+public class User {
+
+    public void eat(){
+        System.out.println("吃饭了");
+    }
+
+}
---
+++ src/test/java/test/UserTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import com.wmh.bean.User;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @ClassName: UserTest
+ * @Description: TODO
+ * @Author: isiy
+ * @Date: 2023/6/3 0:18
+ * @Version: 1.0
+ **/
+public class UserTest {
+
+    @Test
+    public void testUser(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        User userBean = applicationContext.getBean("userBean",User.class);
+        userBean.eat();
+        Logger logger1 = LoggerFactory.getLogger(UserTest.class);
+        logger1.debug("debug信息");
+
+    }
+
+    @Test
+    public void testUserClss(){
+       
+
+    }
+
+}
